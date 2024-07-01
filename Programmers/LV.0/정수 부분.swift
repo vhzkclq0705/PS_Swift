3d0ebaeebb549570b660e593bbf24677af3cfa07
oh_jun <vhzkclq0705@naver.com>
2024-07-01 18:49:57 +0900

    정수 부분

---
+++ Programmers/LV.0/정수 부분.swift
@@ -0,0 +1,14 @@
+//
+//  정수 부분.swift
+//  PS_Swift
+//
+//  Created by 권오준 on 2024-07-01.
+//
+
+import Foundation
+
+func 정수부분() {
+    func solution(_ flo:Double) -> Int {
+        return Int(flo)
+    }
+}
