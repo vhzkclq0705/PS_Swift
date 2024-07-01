ca95fbf91e8edbb906c507b8e0e120a20c1493e2
oh_jun <vhzkclq0705@naver.com>
2024-07-01 18:59:14 +0900

    문자열을 정수로 변환하기

---
+++ Programmers/LV.0/문자열을 정수로 변환하기.swift
@@ -0,0 +1,14 @@
+//
+//  문자열을 정수로 변환하기.swift
+//  PS_Swift
+//
+//  Created by 권오준 on 2024-07-01.
+//
+
+import Foundation
+
+func 문자열을정수로변환하기() {
+    func solution(_ n_str:String) -> Int {
+        return Int(n_str)!
+    }
+}
