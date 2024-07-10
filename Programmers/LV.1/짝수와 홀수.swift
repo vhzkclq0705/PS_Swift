325472f6b32d0561de0ae35385be0fd1d2c4dc65
oh_jun <vhzkclq0705@naver.com>
2024-07-10 20:12:41 +0900

    짝수와 홀수

---
+++ Programmers/LV.1/짝수와 홀수.swift
@@ -0,0 +1,12 @@
+//
+//  짝수와 홀수.swift
+//  PS_Swift
+//
+//  Created by 권오준 on 2024-07-10.
+//
+
+import Foundation
+
+fileprivate func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
